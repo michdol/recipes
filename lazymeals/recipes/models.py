fe7ae77dc4f56d403ed7951e23cc5463b553923f
michdol <michal.dolbniak@gmail.com>
2019-03-07 09:26:21 +0900

    Created models

--- lazymeals/recipes/models.py
+++ lazymeals/recipes/models.py
@@ -1,3 +1,29 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
-# Create your models here.
+
+class SourceWebsite(models.Model):
+	url = models.CharField(max_length=1024, verbose_name="url")
+	is_active = models.BooleanField(default=True, verbose_name="is active")
+
+	class Meta:
+		verbose_name = "source website"
+		verbose_name_plural = "source websites"
+
+
+class Recipe(models.Model):
+	source = models.ForeignKey(SourceWebsite, on_delete=models.DO_NOTHING, related_name="recipes",
+							   verbose_name="source website")
+	name = models.CharField(max_length=256, verbose_name="name")
+	url = models.CharField(max_length=1024, blank=True, verbose_name="url")
+	ingredients = models.TextField(verbose_name="ingredients")
+	directions = models.TextField(verbose_name="directions")
+	ingredients_json = JSONField(default=list, verbose_name="ingredients json")
+	directions_json = JSONField(default=list, verbose_name="directions json")
+	image_url = models.CharField(max_length=1024, blank=True, verbose_name="image url")
+	created = models.DateTimeField(auto_now_add=True, verbose_name="created")
+	updated = models.DateTimeField(auto_now=True, verbose_name="updated")
+
+	class Meta:
+		verbose_name = "recipe"
+		verbose_name_plural = "recipes"
--- lazymeals/users/models.py
+++ lazymeals/users/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+	image_url = models.CharField(max_length=256, blank=True, verbose_name="image url")
+
+	class Meta:
+		verbose_name = "user"
+		verbose_name_plural = "users"
